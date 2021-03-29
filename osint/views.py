b5f7bf71e440d7e743426f15badbfe7421db3e1e
adarshvs <mannarakaladarsh@gmail.com>
2021-03-29 18:15:50 +0530

    added html view for analyse page

--- osint/views.py
+++ osint/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return HttpResponse('OPEN SOUCE INTELLIGENCE GATHERING')
 def analyse(request):
-    return HttpResponse('enter mobile number')
+    return render(request,'analyse.html')
--- osint_kepa/settings.py
+++ osint_kepa/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'osint',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'osint_kepa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'osint\template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
