fb9942118311b7d173f4d66407e162e5a554b4f1
adarshvs <mannarakaladarsh@gmail.com>
2021-04-02 23:35:55 +0530

    truecaller class file called in view.py for reducing code size

--- osint/views.py
+++ osint/views.py
@@ -1,57 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-#from osint.includes.classes.truecaller_search_class import Truecaller
+from osint.Includes.classes.truecaller_search_class import Truecaller
 import requests
 import json
 from phonenumbers import carrier
 from phonenumbers import geocoder
 import phonenumbers
 from phonenumbers import timezone
+from .models import TruecallerApiKey
 # Create your views here.
 def index(request):
     return HttpResponse('OPEN SOUCE INTELLIGENCE GATHERING')
 def analyse(request):
+    key = TruecallerApiKey.objects.all()
     num1 = str(request.POST.get('search'))
+    token ="Bearer a1i0R--QULj06V5kbAlVPy_aynMfCnoUHbndb2k01j2bzL9nMP1y8Ti1a5o5xNle"
     if num1 == 'None':
-        num1 = '9048903190'
-        token ="Bearer a1i0R--QULj06V5kbAlVPy_aynMfCnoUHbndb2k01j2bzL9nMP1y8Ti1a5o5xNle"
-        turl = "https://search5-noneu.truecaller.com/v2/search?q=" + num1 + "&countryCode=IN&type=4&locAddr=&placement=SEARCHRESULTS%2CHISTORY%2CDETAILS&encoding=json"
-        theaders = {
-                "user-agent": "Truecaller/11.5.7 (Android;10)",
-                "Accept-Encoding": "gzip",
-                "authorization": token,
-                "Host": "search5-noneu.truecaller.com"
-            }
-        tresponse = requests.get(turl, headers=theaders, timeout=5)
-        output = tresponse
-        j = output.json()
-        name = j['data'][0]['name'] 
-        if j['data'][0]['internetAddresses']:
-            email = j['data'][0]['internetAddresses'][0]['id']
-        elif not j['data'][0]['internetAddresses']:
-            email = "not available"
-        carrier = j['data'][0]['phones'][0]['carrier']
-        try:
-            gender = j['data'][0]['gender']
-        except KeyError:
-            gender = "gender is unknown"
-       
-        street = j['data'][0]['addresses'][0]['street']
-        city = j['data'][0]['addresses'][0]['city']
-        carrier = j['data'][0]['phones'][0]['carrier']     
-        return render(request,'analyse.html',{"name":name, "num1":num1, "carrier":carrier, "email":email,"gender":gender,"street":street,"city":city})
-    else:
+        return render(request,'analyse.html')
+    else:        
         num1 == num1
-        token ="Bearer a1i0R--QULj06V5kbAlVPy_aynMfCnoUHbndb2k01j2bzL9nMP1y8Ti1a5o5xNle"
-        turl = "https://search5-noneu.truecaller.com/v2/search?q=" + num1 + "&countryCode=IN&type=4&locAddr=&placement=SEARCHRESULTS%2CHISTORY%2CDETAILS&encoding=json"
-        theaders = {
-                "user-agent": "Truecaller/11.5.7 (Android;10)",
-                "Accept-Encoding": "gzip",
-                "authorization": token,
-                "Host": "search5-noneu.truecaller.com"
-            }
-        tresponse = requests.get(turl, headers=theaders, timeout=5)
-        output = tresponse
+        true_caller_result = Truecaller(num1, token)
+        output = true_caller_result.truecaller_search()
         j = output.json()
         name = j['data'][0]['name']
         if j['data'][0]['internetAddresses']:
@@ -69,6 +38,6 @@ def analyse(request):
             street = "not known"
         city = j['data'][0]['addresses'][0]['city']
         carrier = j['data'][0]['phones'][0]['carrier']     
-        return render(request,'analyse.html',{"name":name, "num1":num1, "carrier":carrier, "email":email,"gender":gender,"street":street,"city":city})
+        return render(request,'analyse.html',{"name":name, "num1":num1, "carrier":carrier, "email":email,"gender":gender,"street":street,"city":city,"key":key})
     
    
