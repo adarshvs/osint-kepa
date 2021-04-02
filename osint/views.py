5863883799f93df2d19452f5f89f841890d1f1fd
adarshvs <mannarakaladarsh@gmail.com>
2021-04-02 14:15:45 +0530

    gender getting field from truecaller removed

--- osint/views.py
+++ osint/views.py
@@ -12,19 +12,40 @@ def index(request):
     return HttpResponse('OPEN SOUCE INTELLIGENCE GATHERING')
 def analyse(request):
     num1 = str(request.POST.get('search'))
-    token ="Bearer a1i0R--QULj06V5kbAlVPy_aynMfCnoUHbndb2k01j2bzL9nMP1y8Ti1a5o5xNle"
-    turl = "https://search5-noneu.truecaller.com/v2/search?q=" + num1 + "&countryCode=IN&type=4&locAddr=&placement=SEARCHRESULTS%2CHISTORY%2CDETAILS&encoding=json"
-    theaders = {
-            "user-agent": "Truecaller/11.5.7 (Android;10)",
-            "Accept-Encoding": "gzip",
-            "authorization": token,
-            "Host": "search5-noneu.truecaller.com"
-        }
-    tresponse = requests.get(turl, headers=theaders, timeout=5)
-    output = tresponse
-    j = output.json()
-    name = j['data'][0]['name']
-    p = phonenumbers.parse("+91"+num1)
-    isp = carrier.name_for_valid_number(p,'en')
-    gender = j['data'][0]['gender']
-    return render(request,'analyse.html',{"name":name, "num1":num1, "isp":isp, "gender":gender})
+    if num1 == 'None':
+        num1 = '9048903190'
+        token ="Bearer a1i0R--QULj06V5kbAlVPy_aynMfCnoUHbndb2k01j2bzL9nMP1y8Ti1a5o5xNle"
+        turl = "https://search5-noneu.truecaller.com/v2/search?q=" + num1 + "&countryCode=IN&type=4&locAddr=&placement=SEARCHRESULTS%2CHISTORY%2CDETAILS&encoding=json"
+        theaders = {
+                "user-agent": "Truecaller/11.5.7 (Android;10)",
+                "Accept-Encoding": "gzip",
+                "authorization": token,
+                "Host": "search5-noneu.truecaller.com"
+            }
+        tresponse = requests.get(turl, headers=theaders, timeout=5)
+        output = tresponse
+        j = output.json()
+        name = j['data'][0]['name']
+        p = phonenumbers.parse("+91"+num1)
+        isp = carrier.name_for_valid_number(p,'en')
+        #gender = j['data'][0]['gender']
+        return render(request,'analyse.html',{"name":name, "num1":num1, "isp":isp})
+    else:
+        num1 == num1
+        token ="Bearer a1i0R--QULj06V5kbAlVPy_aynMfCnoUHbndb2k01j2bzL9nMP1y8Ti1a5o5xNle"
+        turl = "https://search5-noneu.truecaller.com/v2/search?q=" + num1 + "&countryCode=IN&type=4&locAddr=&placement=SEARCHRESULTS%2CHISTORY%2CDETAILS&encoding=json"
+        theaders = {
+                "user-agent": "Truecaller/11.5.7 (Android;10)",
+                "Accept-Encoding": "gzip",
+                "authorization": token,
+                "Host": "search5-noneu.truecaller.com"
+            }
+        tresponse = requests.get(turl, headers=theaders, timeout=5)
+        output = tresponse
+        j = output.json()
+        name = j['data'][0]['name']
+        p = phonenumbers.parse("+91"+num1)
+        isp = carrier.name_for_valid_number(p,'en')
+        #gender = j['data'][0]['gender']
+        return render(request,'analyse.html',{"name":name, "num1":num1, "isp":isp})
+   
