5762ceea1ffd1ba592190179c64c0c591ff2c59d
adarshvs <mannarakaladarsh@gmail.com>
2021-04-04 21:52:24 +0530

    extended User model with Profile model

--- osint/admin.py
+++ osint/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import TruecallerApiKey
 from .models import TruecallerDetails
+from .models import Profile
 # Register your models here.
 
 admin.site.register(TruecallerApiKey)
 admin.site.register(TruecallerDetails)
+admin.site.register(Profile)
