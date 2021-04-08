56947d4982ff06a6ebdffc68cd219b158b8b9277
adarshvs <mannarakaladarsh@gmail.com>
2021-04-08 17:00:03 +0530

    default media path and content changed

--- osint_kepa/settings.py
+++ osint_kepa/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR,'Images')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
 ]
