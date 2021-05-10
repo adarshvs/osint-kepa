a0dcbb68ae8f29a75f7e6a3e969830d89177a467
adarshvs <mannarakaladarsh@gmail.com>
2021-05-10 21:07:30 +0530

    image and multimedia extraction added
    image metadata extraction not working tho

--- osint/includes/classes/metapdf.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfFileReader
-class PdfMeta:
-    def pdf_metadata(self):
-        pdf_toread = PdfFileReader(open(self.path, "rb"))
-        pdf_info = pdf_toread.getDocumentInfo()
-        return pdf_info
-    def __init__(self, path):
-        self.path = path
-
