dc93f4a5fddfd4e8d61feb60b533a44ffd89b083
adarshvs <mannarakaladarsh@gmail.com>
2021-06-05 16:53:35 +0530

    heroku domain whitelisted

--- osint_kepa/settings.py
+++ osint_kepa/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '+dtxr=wl0b5oh535b%yhz30$1in376zj!#=$h17$!vj9jzcjy7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','172.16.7.246','osint-kepa.herokuapp.com']
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','172.16.7.246','https://osint-kepa.herokuapp.com']
 
 
 # Application definition
