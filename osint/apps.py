69d0ce407039b691f423089ac3a5eaa73b5e8bc9
adarshvs <mannarakaladarsh@gmail.com>
2021-07-06 17:09:04 +0530

    cleared heroku config conflicts

--- osint/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OsintConfig(AppConfig):
-    name = 'osint'
--- osint/templatetags/onion.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def onion(value):
-    return value.replace("onion","onion.ly")
